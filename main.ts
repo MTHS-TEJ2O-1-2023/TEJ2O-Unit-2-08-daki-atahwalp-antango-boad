3632e50d847e6aa16e2a26c1c77778255301a00d
daki-atahwalp-antango-boad <144264104+daki-atahwalp-antango-boad@users.noreply.github.com>
2023-10-10 13:12:22 -0400

    RGB

--- main.ts
+++ main.ts
@@ -1,8 +1,61 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Daki A.B
+ * Created on: Oct 2023
+ * This program makes all colours of the RGB LED flash
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.pause(1000)
+
+// Input
+
+    // Red
+input.onButtonPressed(Button.A, function () {
+
+  pins.digitalWritePin(DigitalPin.P16, 1)
+  basic.showString('Red')
+  basic.pause(1000)
+  pins.digitalWritePin(DigitalPin.P16, 0)
+// Green
+  pins.digitalWritePin(DigitalPin.P15, 1)
+  basic.showString('Green')
+  basic.pause(1000)
+  pins.digitalWritePin(DigitalPin.P15, 0)
+// Blue
+  pins.digitalWritePin(DigitalPin.P14, 1)
+  basic.showString('Blue')
+  basic.pause(1000)
+  pins.digitalWritePin(DigitalPin.P14, 0)
+// Magenta
+  pins.digitalWritePin(DigitalPin.P16, 1)
+  pins.digitalWritePin(DigitalPin.P14, 1)
+  basic.showString('Magenta')
+  basic.pause(1000)
+  pins.digitalWritePin(DigitalPin.P16, 0)
+  pins.digitalWritePin(DigitalPin.P14, 0)
+// Yellow
+  pins.digitalWritePin(DigitalPin.P16, 1)
+  pins.digitalWritePin(DigitalPin.P15, 1)
+  basic.showString('Yellow')
+  basic.pause(1000)
+  pins.digitalWritePin(DigitalPin.P16, 0)
+  pins.digitalWritePin(DigitalPin.P15, 0)
+// Cyan
+  pins.digitalWritePin(DigitalPin.P16, 1)
+  pins.digitalWritePin(DigitalPin.P14, 1)
+  basic.showString('Cyan')
+  basic.pause(1000)
+  pins.digitalWritePin(DigitalPin.P16, 0)
+  pins.digitalWritePin(DigitalPin.P14, 0)
+// White
+  pins.digitalWritePin(DigitalPin.P16, 1)
+  pins.digitalWritePin(DigitalPin.P14, 1)
+  pins.digitalWritePin(DigitalPin.P15, 1)
+  basic.showString('White')
+  basic.pause(1000)
+  pins.digitalWritePin(DigitalPin.P16, 0)
+  pins.digitalWritePin(DigitalPin.P15, 0)
+  pins.digitalWritePin(DigitalPin.P14, 0)
+   
+    })
