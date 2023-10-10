95bb9a1806f8d310a1256057d691a33c0a727d2b
daki-atahwalp-antango-boad <144264104+daki-atahwalp-antango-boad@users.noreply.github.com>
2023-10-10 21:39:02 +0000

    RGB LED

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,56 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Daki A.B
+Created on: Oct 2023
+This module is a Micro:bit flashing all RGB LED colours
 """
 
 from microbit import *
 
 
-display.scroll("Hello, World!")
+while True:
+    if button_a.is_prerssed():
+        # Red
+        pin10.write_digital(1)
+        display.scroll("Red")
+        sleep(1)
+        pin10.write_digital(0)
+        # Green
+        pin19.write_digital(1)
+        display.scroll("Green")
+        sleep(1)
+        pin19.write_digital(0)
+        # Blue
+        pin20.write_digital(1)
+        display.scroll("Blue")
+        sleep(1)
+        pin20.write_digital(0)
+        # Magenta
+        pin10.write_digital(1)
+        pin20.write_digital(1)
+        display.scroll("Magenta")
+        sleep(1)
+        pin10.write_digital(0)
+        pin20.write_digital(0)
+        # Yellow
+        pin10.write_digital(1)
+        pin19.write_digital(1)
+        display.scroll("Yellow")
+        sleep(1)
+        pin10.write_digital(0)
+        pin19.write_digital(0)
+        # Cyan
+        pin19.write_digital(1)
+        pin20.write_digital(1)
+        display.scroll("Cyan")
+        sleep(1)
+        pin19.write_digital(0)
+        pin20.write_digital(0)
+        # White
+        pin10.write_digital(1)
+        pin19.write_digital(1)
+        pin20.write_digital(1)
+        display.scroll("White")
+        sleep(1)
+        pin10.write_digital(0)
+        pin19.write_digital(0)
+        pin20.write_digital(0)
