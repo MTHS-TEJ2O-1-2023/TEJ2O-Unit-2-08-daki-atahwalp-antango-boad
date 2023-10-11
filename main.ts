185b4ab511a9524d15644d4343e0799d42cc6760
daki-atahwalp-antango-boad <144264104+daki-atahwalp-antango-boad@users.noreply.github.com>
2023-10-11 13:16:34 -0400

    RGB LED

--- main.ts
+++ main.ts
@@ -14,55 +14,55 @@ basic.pause(1000)
 input.onButtonPressed(Button.A, function () {
     
   // Red
-  pins.digitalWritePin(DigitalPin.P10, 1)
+  pins.digitalWritePin(DigitalPin.P14, 1)
   basic.showString('Red')
   basic.pause(1000)
-  pins.digitalWritePin(DigitalPin.P10, 0)
+  pins.digitalWritePin(DigitalPin.P14, 0)
   
   // Green
-  pins.digitalWritePin(DigitalPin.P19, 1)
+  pins.digitalWritePin(DigitalPin.P15, 1)
   basic.showString('Green')
   basic.pause(1000)
-  pins.digitalWritePin(DigitalPin.P19, 0)
+  pins.digitalWritePin(DigitalPin.P15, 0)
   
   // Blue
-  pins.digitalWritePin(DigitalPin.P20, 1)
+  pins.digitalWritePin(DigitalPin.P16, 1)
   basic.showString('Blue')
   basic.pause(1000)
-  pins.digitalWritePin(DigitalPin.P20, 0)
+  pins.digitalWritePin(DigitalPin.P16, 0)
   
   // Magenta
-  pins.digitalWritePin(DigitalPin.P10, 1)
-  pins.digitalWritePin(DigitalPin.P20, 1)
+  pins.digitalWritePin(DigitalPin.P14, 1)
+  pins.digitalWritePin(DigitalPin.P16, 1)
   basic.showString('Magenta')
   basic.pause(1000)
-  pins.digitalWritePin(DigitalPin.P10, 0)
-  pins.digitalWritePin(DigitalPin.P20, 0)
+  pins.digitalWritePin(DigitalPin.P14, 0)
+  pins.digitalWritePin(DigitalPin.P16, 0)
   
   // Yellow
-  pins.digitalWritePin(DigitalPin.P10, 1)
-  pins.digitalWritePin(DigitalPin.P19, 1)
+  pins.digitalWritePin(DigitalPin.P14, 1)
+  pins.digitalWritePin(DigitalPin.P15, 1)
   basic.showString('Yellow')
   basic.pause(1000)
-  pins.digitalWritePin(DigitalPin.P10, 0)
-  pins.digitalWritePin(DigitalPin.P19, 0)
+  pins.digitalWritePin(DigitalPin.P14, 0)
+  pins.digitalWritePin(DigitalPin.P15, 0)
   
   // Cyan
-  pins.digitalWritePin(DigitalPin.P10, 1)
-  pins.digitalWritePin(DigitalPin.P20, 1)
+  pins.digitalWritePin(DigitalPin.P14, 1)
+  pins.digitalWritePin(DigitalPin.P16, 1)
   basic.showString('Cyan')
   basic.pause(1000)
-  pins.digitalWritePin(DigitalPin.P10, 0)
-  pins.digitalWritePin(DigitalPin.P20, 0)
+  pins.digitalWritePin(DigitalPin.P14, 0)
+  pins.digitalWritePin(DigitalPin.P16, 0)
   
   // White
-  pins.digitalWritePin(DigitalPin.P10, 1)
-  pins.digitalWritePin(DigitalPin.P20, 1)
-  pins.digitalWritePin(DigitalPin.P19, 1)
+  pins.digitalWritePin(DigitalPin.P14, 1)
+  pins.digitalWritePin(DigitalPin.P15, 1)
+  pins.digitalWritePin(DigitalPin.P16, 1)
   basic.showString('White')
   basic.pause(1000)
-  pins.digitalWritePin(DigitalPin.P10, 0)
-  pins.digitalWritePin(DigitalPin.P19, 0)
-  pins.digitalWritePin(DigitalPin.P20, 0)
+  pins.digitalWritePin(DigitalPin.P14, 0)
+  pins.digitalWritePin(DigitalPin.P15, 0)
+  pins.digitalWritePin(DigitalPin.P16, 0)
    
     })
